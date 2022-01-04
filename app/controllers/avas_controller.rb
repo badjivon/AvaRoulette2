b19d88c022642895f47a70da9adeacc3f69f5589
Brice ADJIVON <badjivon@fastmail.com>
2022-01-05 00:38:04 +0100

    Création page roulette et simpleform

--- app/controllers/avas_controller.rb
+++ app/controllers/avas_controller.rb
@@ -1,2 +1,22 @@
 class AvasController < ApplicationController
+  def index
+    @avas = Ava.all
+  end
+
+  def roulette
+    @avas = Ava.all
+  end
+
+  def new
+    @ava = Ava.new
+  end
+
+  def create
+    @ava = Ava.new(ava_params)
+    if @ava.save
+      redirect_to avas_path
+    else
+      render 'ava/new'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_231039) do
+ActiveRecord::Schema.define(version: 2022_01_04_231244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2022_01_04_231039) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "avas", force: :cascade do |t|
+    t.integer "number"
+    t.string "rare"
+    t.integer "batch"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
